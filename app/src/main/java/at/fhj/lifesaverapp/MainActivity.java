93fd0d45960e36fa8045be3329c4e9a3392defae
anja-05 <a-schloffer@gmx.at>
2025-05-17 13:07:15 +0200

    Bottom Navigation programmed

--- app/src/main/java/at/fhj/lifesaverapp/MainActivity.java
+++ app/src/main/java/at/fhj/lifesaverapp/MainActivity.java
@@ -4,9 +4,10 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import at.fhj.lifesaverapp.R;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment;
+            switch (item.getItemId()) {
+                case R.id.:
+                    selectedFragment = new UebungFragment();
+                    break;
+                case R.id.nav_profil:
+                    selectedFragment = new ProfilFragment();
+                    break;
+                case R.id.nav_lernen:
+                default:
+                    selectedFragment = new LernenFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+            return true;
+        });
+
+        bottomNav.setSelectedItemId(R.id.nav_lernen);
 
     }
 }
